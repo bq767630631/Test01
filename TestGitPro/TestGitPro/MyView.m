c4831af69270b0e38cd49bfd94204c76ff3241f3
baoqiang <767630631@qq.com>
2018-12-07 14:27:51 +0800

    new view

---
+++ TestGitPro/TestGitPro/MyView.m
@@ -0,0 +1,21 @@
+//
+//  MyView.m
+//  TestGitPro
+//
+//  Created by 包强 on 2018/12/7.
+//  Copyright © 2018 包强. All rights reserved.
+//
+
+#import "MyView.h"
+
+@implementation MyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
